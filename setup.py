aa13cf899052ea6aad9626ff36bd10bc1e3f93df
ricardo-valdivia-im <ricardo.valdivia@impresee.com>
2023-10-19 19:08:22 -0300

    Initial commit with dsclassifier included

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README_DSGD.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dsgd',
+    version='0.2',
+    author='Sergio Peñafiel',
+    description='Tabular interpretable classifier based on Dempster-Shafer\
+                 Theory and Gradient Descent',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sergio-P/DSGD',
+    packages=['dsgd'],
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'torch',
+        'dill'
+    ]
+)
+
+# setuptools.setup(
+#     name='cdsgd',
+#     version='0.1',
+#     author='Ricardo Valdivia',
+#     description='Tabular interpretable clustering based on Dempster-Shafer\
+#                  Theory and Gradient Descent',
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     url='https://github.com/ricardo-valdivia/CDSGD',
+#     packages=['cdsgd'],
+#     include_package_data=True,
+#     install_requires=[
+#         'numpy',
+#         'pandas',
+#         'scikit-learn',
+#         'torch',
+#         'dill'
+#     ]
+# )
