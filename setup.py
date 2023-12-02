0b9866e48c81c8aaf3697e6dec0363943b386185
ricardo-valdivia <ricardo.valdivia22@gmail.com>
2023-12-02 20:33:37 -0300

    [Change] setup and git ignore

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ import setuptools
 with open('README_DSGD.md', 'r') as fh:
     long_description = fh.read()
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='dsgd',
     version='0.2',
@@ -28,22 +25,25 @@ setuptools.setup(
     ]
 )
 
-# setuptools.setup(
-#     name='cdsgd',
-#     version='0.1',
-#     author='Ricardo Valdivia',
-#     description='Tabular interpretable clustering based on Dempster-Shafer\
-#                  Theory and Gradient Descent',
-#     long_description=long_description,
-#     long_description_content_type='text/markdown',
-#     url='https://github.com/ricardo-valdivia/CDSGD',
-#     packages=['cdsgd'],
-#     include_package_data=True,
-#     install_requires=[
-#         'numpy',
-#         'pandas',
-#         'scikit-learn',
-#         'torch',
-#         'dill'
-#     ]
-# )
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cdsgd',
+    version='0.1',
+    author='Ricardo Valdivia',
+    description='Tabular interpretable clustering based on Dempster-Shafer\
+                 Theory and Gradient Descent',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ricardo-valdivia/CDSGD',
+    packages=['cdsgd'],
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'torch',
+        'dill'
+    ]
+)
